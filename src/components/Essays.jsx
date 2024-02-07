d5ca58d784a4520ba0cfd83c5e41eccf20f03d77
sanjana adiga <sadiga@uwo.ca>
2024-02-07 13:05:44 -0500

    initial commit

---
+++ src/components/Essays.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Essays() {
+    const openPDF = () => {
+        window.open('src/docs/Resume-SanjanaAdiga.pdf', '_blank');
+      };
+    
+    return (
+        <div id="essays">
+            <h2>writing</h2>
+            <p>Intro to AI...</p>
+            
+        </div>
+    )
+}
